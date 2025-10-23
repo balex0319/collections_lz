fb33850f9395cd909fc924881604f38b8a64501a
balex0319 <borissskk@tutamail.com>
2025-10-23 17:44:24 +0300

    plot.py

--- plot.py
+++ plot.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
-plt.plot(x, y, label="sin(x)", color="blue") 
+x = np.linspace(0, 30, 100)
+y = x**3
+plt.plot(x, y, label="x**3", color="blue") 
 plt.show()
